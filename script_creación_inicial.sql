0f5e6c12a4e56f4340a5d2e28380b085d6347ecd
Franciscopanozzo <62455934+Franpanozzo@users.noreply.github.com>
2021-10-15 00:46:48 -0300

    Update script_creación_inicial.sql

--- script_creación_inicial.sql
+++ script_creación_inicial.sql
@@ -1,3 +1,40 @@
+IF not exists (select * from sys.schemas where name = 'LOS_GEDEDES')
+BEGIN
+	exec ('CREATE SCHEMA LOS_GEDEDES')
+END
+GO
+
+/*Me fijo si existe el esquema en la tabla del sistema si no existe lo creo.
+--------------------------------------------------------------------------
+Me fijo primero si existe el elemento en la tabla, si la tabla ya existe la dropea para despues poder correr el create.  (ANTES DE CADA CREADO IRIA)
+
+IF EXISTS (select * from sys.objects where object_id = OBJECT_ID(N'[NOMBRE_ESQUEMA_TP].TABLA') and type = 'U')
+	DROP TABLE [NOMBRE_ESQUEMA_TP].TABLA
+	GO
+
+-- CREACION DE TABLAS 
+-- CREACION DE INDICES
+-- CREACION DE VISTAS
+-- CREACION DE FUNCIONES
+-- CREACION DE SP
+-- CREACION DE TRIGGERS
+-- LLENADO DE TABLAS / EXEC DE SP (UNO POR TABLA)
+-- CREACION DE INDICES
+
+GO
+
+TENER EN CUENTA
+	1. DEPENDENCIAS 
+	2. PERFORMANCE (TIEMPO MAXIMO DE LA CREACION 5 MINUTOS)
+		-CUANDO CREAR LOS INDICES
+		-CURSORES
+	3. TAMAÑO DE LAS TRANSACCIONES (Mantenerlas lo mas chicas posibles. COMMITEAR CUANDO SE LLENA LA TABLA PARA TENER UN CONTROL EN CASO DE TENER QUE ROLLBACKEAR)
+	4. FORMATO FECHA/HORA --*/
+  
+  
+------------- CREACION DE TABLAS -----------------------
+--faltan drops
+
 CREATE TABLE LOS_GEDEDES.Camion(
 patente NVARCHAR(255),
 nroChasis NVARCHAR(255),
