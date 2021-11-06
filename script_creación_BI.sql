103b54edfb20528639097db43110e12b070e120e
SantiagoCarnezis <62964148+SantiagoCarnezis@users.noreply.github.com>
2021-11-06 20:19:11 -0300

    tablas santi

--- script_creación_BI.sql
+++ script_creación_BI.sql
@@ -11,6 +11,15 @@ IF EXISTS (select * from sys.objects where object_id = OBJECT_ID('LOS_GEDEDES.BI
   
 IF EXISTS (select * from sys.objects where object_id = OBJECT_ID('LOS_GEDEDES.BI_dimension_recorrido') and type = 'U')
 	DROP TABLE LOS_GEDEDES.BI_dimension_recorrido
+
+IF EXISTS (select * from sys.objects where object_id = OBJECT_ID('LOS_GEDEDES.BI_tarea') and type = 'U')
+	DROP TABLE LOS_GEDEDES.BI_tarea
+
+IF EXISTS (select * from sys.objects where object_id = OBJECT_ID('LOS_GEDEDES.BI_material') and type = 'U')
+	DROP TABLE LOS_GEDEDES.BI_material
+
+IF EXISTS (select * from sys.objects where object_id = OBJECT_ID('LOS_GEDEDES.BI_mecanico') and type = 'U')
+	DROP TABLE LOS_GEDEDES.BI_mecanico
  
   
 ------------ CREACION DE TABLAS ----------------
@@ -51,9 +60,42 @@ nroRecorrido	INT IDENTITY(1,1),
 precioRecorrido	DECIMAL(18,2),
 ciudadOrigen	NVARCHAR(255),   /*Desnormalizo las ciudades entendiendo que no se pueden poner FKs en la dimension*/
 ciudadDestino	NVARCHAR(255),
-recorridoKM	INT,
+recorridoKM		INT,
 PRIMARY KEY (nroRecorrido)
 );
+
+
+CREATE TABLE LOS_GEDEDES.BI_tarea(
+codigo			INT,
+descripcion		NVARCHAR(255),
+tipo			NVARCHAR(255),
+fechaInicio			DATETIME2(3),
+fechaFin			DATETIME2(3),
+PRIMARY KEY (codigo)
+)
+
+
+CREATE TABLE LOS_GEDEDES.BI_material(
+codigo			NVARCHAR(100),
+descripcion		NVARCHAR(255),
+precioBase		DECIMAL(18,0),
+cantidad		INT,
+PRIMARY KEY (codigo)
+)
+
+
+CREATE TABLE LOS_GEDEDES.BI_mecanico(
+nroLegajo	INT,
+nombre		NVARCHAR(255),
+apellido	NVARCHAR(255),
+dni			DECIMAL(18,0),
+direccion	NVARCHAR(255),
+telefono	INT,
+mail		NVARCHAR(255),
+fecha_nac	DATETIME2(3),
+costo_hora	INT,
+PRIMARY KEY (nroLegajo),
+)
 
 
 
