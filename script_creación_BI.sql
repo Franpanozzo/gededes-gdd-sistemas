e45984903bf3f3a44b8b1e481e35a1492a8cde45
Franciscopanozzo <62455934+Franpanozzo@users.noreply.github.com>
2021-11-06 19:07:15 -0300

    Update script_creación_BI.sql

--- script_creación_BI.sql
+++ script_creación_BI.sql
@@ -19,40 +19,40 @@ CREATE TABLE LOS_GEDEDES.BI_viaje(
 choferLegajo INT,
 patenteCamion	NVARCHAR(255),
 recorrido	INT,
-tiempo INT
-nroPaquete INT
+tiempo INT,
+nroPaquete INT,
 precioRecorrido INT,
 naftaConsumida	DECIMAL(18,2),
-PRIMARY KEY (choferLegajo, patenteCamion, recorrido, tiempo, nroPaquete)
+PRIMARY KEY (choferLegajo, patenteCamion, recorrido, tiempo, nroPaquete),
 FOREIGN KEY (nroPaquete) REFERENCES LOS_GEDEDES.BI_dimension_paquete,
 FOREIGN KEY (patenteCamion) REFERENCES LOS_GEDEDES.BI_dimension_camion,
 FOREIGN KEY (choferLegajo) REFERENCES LOS_GEDEDES.BI_dimension_chofer,
 FOREIGN KEY (recorrido) REFERENCES LOS_GEDEDES.BI_dimension_recorrido,
-FOREIGN KEY (tiempo) REFERENCES LOS_GEDEDES.BI_dimension_tiempo,
+FOREIGN KEY (tiempo) REFERENCES LOS_GEDEDES.BI_dimension_tiempo
 );
 
-CREATE TABLE BI_dimension_camion(
+CREATE TABLE LOS_GEDEDES.BI_dimension_camion(
 patente		NVARCHAR(255),
 nroChasis	NVARCHAR(255),
 nroMotor	NVARCHAR(255),
 fechaAlta	DATETIME2(3),
-PRIMARY KEY (patente),
+PRIMARY KEY (patente)
 );
 
-CREATE TABLE BI_dimension_tiempo(
+CREATE TABLE LOS_GEDEDES.BI_dimension_tiempo(
 idTiempo INT IDENTITY(1,1),
 anio INT,
 cuatrimestre INT,
-PRIMARY KEY (idTiempo),
+PRIMARY KEY (idTiempo)
 );
 
-CREATE TABLE BI_dimension_recorrido(
+CREATE TABLE LOS_GEDEDES.BI_dimension_recorrido(
 nroRecorrido	INT IDENTITY(1,1),
 precioRecorrido	DECIMAL(18,2),
 ciudadOrigen	NVARCHAR(255),   /*Desnormalizo las ciudades entendiendo que no se pueden poner FKs en la dimension*/
 ciudadDestino	NVARCHAR(255),
 recorridoKM	INT,
-PRIMARY KEY (nroRecorrido),
+PRIMARY KEY (nroRecorrido)
 );
 
 
