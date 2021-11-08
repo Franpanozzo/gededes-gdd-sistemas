30f7fc63bae257b87f1c3fc5f245fc98a1096009
Franciscopanozzo <62455934+Franpanozzo@users.noreply.github.com>
2021-11-08 00:44:16 -0300

    Update script_creación_BI.sql

--- script_creación_BI.sql
+++ script_creación_BI.sql
@@ -1,3 +1,49 @@
+------------- FUNCIONES AUXILIARES --------------
+
+IF EXISTS (select * from sys.objects where object_id = OBJECT_ID('LOS_GEDEDES.rangoEtario_fx') and type = 'FN')
+	DROP FUNCTION LOS_GEDEDES.rangoEtario_fx
+
+IF EXISTS (select * from sys.objects where object_id = OBJECT_ID('LOS_GEDEDES.cuatrimestre_fx') and type = 'FN')
+	DROP FUNCTION LOS_GEDEDES.cuatrimestre_fx
+
+
+CREATE FUNCTION LOS_GEDEDES.rangoEtario_fx(@fecha_nac DATETIME2(3))
+RETURNS NVARCHAR(15)
+AS
+BEGIN
+	DECLARE @rango NVARCHAR(15)
+	DECLARE @edad INT
+
+	SELECT @edad = FLOOR(
+		(cast(convert(varchar(8),getdate(),112) as int) -
+		cast(convert(varchar(8),@fecha_nac,112) as int) ) / 10000
+		)
+
+	SELECT @rango = CASE 
+		WHEN @edad >= 18 AND @edad <= 30 THEN '18 - 30'
+		WHEN @edad > 30 AND @edad <= 50 THEN '30 - 50'
+		ELSE '50+'
+		END
+
+	RETURN @rango
+END;
+
+CREATE FUNCTION LOS_GEDEDES.cuatrimestre_fx(@fecha DATETIME2(3))
+RETURNS INT
+AS BEGIN
+	DECLARE @cuatrimetre INT
+	DECLARE @numeroMes INT
+
+	SELECT @numeroMes = MONTH(@fecha)
+
+	SELECT @cuatrimestre = CASE
+		WHEN @numeroMes >= 1 AND @numeroMes =< 4 THEN 1
+		WHEN @numeroMes > 4 AND @numeroMes <= 8 THEN 2 
+		WHEN @numeroMes > 8 AND @numeroMes <= 12 THEN 3 
+	
+	RETURN @cuatrimestre
+END;
+
 ------------- DROP TABLAS -----------------------
 
 IF EXISTS (select * from sys.objects where object_id = OBJECT_ID('LOS_GEDEDES.BI_viaje') and type = 'U')
@@ -55,18 +101,17 @@ mail			NVARCHAR(255),
 fecha_nac		DATETIME2(3),
 costo_hora		INT,
 rangoEtario 	NVARCHAR(15),
---fechaViaje DATETIME2(3) 
-/*Lo comento porque no se si va aca, mepa que en la tabla de bi viaje,
-quiza dentro de la dimension tiempo*/
 PRIMARY KEY (nroLegajo)
 );
 
+/*
 CREATE TABLE LOS_GEDEDES.BI_dimension_OT(
 nroOrden		INT, 
 estado			NVARCHAR(255),
-fechaCarga		NVARCHAR(255), /*Quiza con la dimension tiempo no sea necesario este campo*/
+fechaCarga		NVARCHAR(255), Quiza con la dimension tiempo no sea necesario este campo
 PRIMARY KEY (nroOrden)
 );
+*/
 
 CREATE TABLE LOS_GEDEDES.BI_dimension_camion(
 patente		NVARCHAR(255),
@@ -77,8 +122,9 @@ PRIMARY KEY (patente)
 );
 
 CREATE TABLE LOS_GEDEDES.BI_dimension_tiempo(
-idTiempo		INT,
+idTiempo		INT IDENTITY(1,1),
 anio			INT,
+mes				INT,
 cuatrimestre	INT,
 PRIMARY KEY (idTiempo)
 );
@@ -86,12 +132,11 @@ PRIMARY KEY (idTiempo)
 CREATE TABLE LOS_GEDEDES.BI_dimension_recorrido(
 nroRecorrido	INT,
 precioRecorrido	DECIMAL(18,2),
-ciudadOrigen	NVARCHAR(255),  
+ciudadOrigen	NVARCHAR(255), 
 ciudadDestino	NVARCHAR(255),
 recorridoKM		INT,
 PRIMARY KEY (nroRecorrido)
 );
-
 
 CREATE TABLE LOS_GEDEDES.BI_dimension_tarea(
 codigo			INT,
@@ -118,7 +163,7 @@ dni			DECIMAL(18,0),
 direccion	NVARCHAR(255),
 telefono	INT,
 mail		NVARCHAR(255),
-rangoEtario	NVARCHAR(15), /*joven, adulto, mayor*/
+rangoEtario	NVARCHAR(15), 
 costo_hora	INT,
 PRIMARY KEY (nroLegajo),
 )
@@ -132,21 +177,20 @@ PRIMARY KEY (codigo),
 CREATE TABLE LOS_GEDEDES.BI_dimension_modelo(
 codigo	INT,
 modelo	NVARCHAR(255),
-capacidadCarga INT,
-capacidadTanque INT,
-velocidadMaxima INT,
 PRIMARY KEY (codigo),
 )
 
 CREATE TABLE LOS_GEDEDES.BI_dimension_paquete(
+id					INT IDENTITY (1,1),
 nroPaquete			INT,
+tipo				NVARCHAR(8),
 largoMax			DECIMAL(18,2),
 pesoMax				DECIMAL(18,2),
 altoMax				DECIMAL(18,2),
 precioBase			DECIMAL(18,2),
 cantidad			INT,
 precioFinalPaquete	DECIMAL(18,2),
-PRIMARY KEY (nroPaquete),
+PRIMARY KEY (id),
 )
 
 CREATE TABLE LOS_GEDEDES.BI_dimension_taller(
@@ -177,15 +221,19 @@ FOREIGN KEY (tiempo) REFERENCES LOS_GEDEDES.BI_dimension_tiempo
 
 CREATE TABLE LOS_GEDEDES.BI_reparacion(
 patenteCamion		NVARCHAR(255),
+codigoMarca			INT,
+codigoModelo		INT,
 choferLegajo 		INT,
 codigoTarea			INT,
 codigoMaterial 		NVARCHAR(100),
 nroOrdenTrabajo 	INT,
 tiempo 				INT,
 idTaller			INT,
-PRIMARY KEY(patenteCamion, choferLegajo, codigoTarea,
+PRIMARY KEY(patenteCamion, codigoMarca, codigoModelo, choferLegajo, codigoTarea,
 codigoMaterial, nroOrdenTrabajo, tiempo, idTaller),
 FOREIGN KEY (patenteCamion) REFERENCES LOS_GEDEDES.BI_dimension_camion,
+FOREIGN KEY (codigoMarca) REFERENCES LOS_GEDEDES.BI_dimension_marca,
+FOREIGN KEY (codigoModelo) REFERENCES LOS_GEDEDES.BI_dimension_modelo,
 FOREIGN KEY (choferLegajo) REFERENCES LOS_GEDEDES.BI_dimension_chofer,
 FOREIGN KEY (codigoTarea) REFERENCES LOS_GEDEDES.BI_dimension_tarea,
 FOREIGN KEY (codigoMaterial) REFERENCES LOS_GEDEDES.BI_dimension_material,
@@ -193,3 +241,42 @@ FOREIGN KEY (nroOrdenTrabajo) REFERENCES LOS_GEDEDES.BI_dimension_OT,
 FOREIGN KEY (tiempo) REFERENCES LOS_GEDEDES.BI_dimension_tiempo,
 FOREIGN KEY (idTaller) REFERENCES LOS_GEDEDES.BI_dimension_taller
 );
+
+----------LLENADO DE LAS DIMENSIONES Y TABLA DE HECHOS-------------
+
+INSERT INTO LOS_GEDEDES.BI_dimension_chofer (nroLegajo,nombre,apellido,dni,direccion,telefono,mail,fecha_nac,costo_hora, rangoEtario)
+SELECT c.nroLegajo, c.nombre, c.apellido, c.dni, c.direccion, c.telefono, c.mail, c.fecha_nac, c.costo_hora, rangoEtario_fx(c.fecha_nac)
+FROM LOS_GEDEDES.Chofer c
+
+INSERT INTO LOS_GEDEDES.BI_dimension_marca (codigo, nombre) 
+SELECT codigo, nombre
+FROM LOS_GEDEDES.Marca
+
+INSERT INTO LOS_GEDEDES.BI_dimension_modelo (codigo, modelo, velocidadMaxima, capacidadTanque, capacidadCarga)
+SELECT codigo, modelo, velocidadMaxima, capacidadTanque, capacidadCarga
+FROM LOS_GEDEDES.Modelo
+
+INSERT INTO LOS_GEDEDES.BI_dimension_paquete (nroPaquete, tipo, largoMax, precioBase, pesoMax, altoMax, precioFinalPaquete, cantidad)
+SELECT tp.nroPaquete, tp.tipo, tp.largoMax, tp.precioBase, tp.pesoMax, tp.altoMax, pv.precioFinalPaquete, pv.cantidad
+FROM LOS_GEDEDES.Paquete_viaje pv 
+JOIN LOS_GEDEDES.Tipo_Paquete tp ON (pv.nroPaquete = tp.nroPaquete)
+
+INSERT INTO LOS_GEDEDES.BI_dimension_tiempo (anio, mes, cuatrimestre)
+SELECT DISTINCT YEAR(fechaInicio), MONTH(fechaInicio), LOS_GEDEDES.cuatrimestre_fx(fechaInicio) FROM LOS_GEDEDES.BI_dimension_tarea
+UNION 
+SELECT DISTINCT YEAR(fechaInicio), MONTH(fechaInicio), LOS_GEDEDES.cuatrimestre_fx(fechaInicio) FROM LOS_GEDEDES.BI_viaje
+
+--SANTI INICIO	
+INSERT INTO LOS_GEDEDES.BI_dimension_taller
+	(id, nombre, direccion, telefono, mail, ciudad)
+SELECT  t.id, t.nombre, t.direccion, t.telefono, t.mail, ci.nombre 
+FROM LOS_GEDEDES.taller t
+	JOIN LOS_GEDEDES.Ciudad ci ON (ci.codigoCiudad = t.codCiudad)
+
+INSERT INTO LOS_GEDEDES.BI_dimension_mecanico
+	(nroLegajo, nombre, apellido, dni, direccion, telefono, mail, rangoEtario, costo_hora)
+SELECT  t.id, t.nombre, t.direccion, t.telefono, t.mail, ci.nombre 
+FROM LOS_GEDEDES.mecanico m
+	JOIN LOS_GEDEDES.Ciudad ci ON (ci.codigoCiudad = t.codCiudad)
+
+--SANTI FIN
